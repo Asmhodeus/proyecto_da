928e7cf6ac84cbfb4027f46ea5791a218fa3daad
Hugo León <haleong@gmail.com>
2026-02-22 14:02:47 -0500

    Prueba manual

--- captura_datos.bat
+++ captura_datos.bat
@@ -1,3 +1,15 @@
 @echo off
-set fecha=%date:~10,4%-%date:~4,2%-%date:~7,2%
-lighthouse https://es.wikipedia.org --output=json --output-path=./report.json
+
+set fecha=%random%
+
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd"') do set fecha=%%i
+
+echo Fecha detectada: %fecha%
+
+lighthouse https://es.wikipedia.org --output=json --output-path=reportes/report_%fecha%.json
+
+git add .
+git commit -a "Reporte %fecha%"
+git push origin master
+
+pause
---
+++ pruebta.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "Prueba manual"
+git push origin master
