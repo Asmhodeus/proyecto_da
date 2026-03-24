58b6d7b383c3d020751dd23eb877b51a5cf8063f
Hugo León <haleong@gmail.com>
2026-03-23 22:30:48 -0500

    Reporte

---
+++ captura_datos_2.bat
@@ -0,0 +1,17 @@
+@echo off
+
+@echo off
+
+echo Ejecutando captura completa...
+
+python captura_metricas_2.py
+
+echo Agregando cambios a git...
+git add .
+echo Haciendo commit...
+git commit -m "Reporte %fecha%"
+echo Haciendo push...
+git push origin master
+
+echo Proceso finalizado
+pause
