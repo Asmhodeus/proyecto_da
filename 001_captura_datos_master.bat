0c0adf39ca543c1a4717c114203cba48eafb86e1
Hugo León <haleong@gmail.com>
2026-04-22 10:33:02 -0500

    Reporte

--- 001_captura_datos_master.bat
+++ 001_captura_datos_master.bat
@@ -1,10 +1,10 @@
 @echo off
 
-echo Ejecutando captura completa...
-
 cd /d "D:\Universidad\10mo Semestre\Big data\Proyecto_desempeño_ambiental"
 
-python "D:\Universidad\10mo Semestre\Big data\Proyecto_desempeño_ambiental\001_captura_metricas_master.py"
+echo Ejecutando captura completa...
+
+python 001_captura_metricas_master.py
 
 echo Agregando cambios a git...
 git add .
